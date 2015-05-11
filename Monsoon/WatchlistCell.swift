92b71c895555174b716606bd46a7ccc8599fc6b5
Bardan Gauchan <bardan.gauchan@gmail.com>
2015-05-11 15:28:16 -0700

    Added a custom tableview cell and made the base layout in storyboard with auto layout setup.

---
+++ Monsoon/WatchlistCell.swift
@@ -0,0 +1,18 @@
+//
+//  WatchlistCell.swift
+//  Monsoon
+//
+//  Created by Bardan Gauchan on 2015-05-04.
+//  Copyright (c) 2015 Bardan Gauchan. All rights reserved.
+//
+
+import UIKit
+import ParseUI
+
+class WatchlistCell: UITableViewCell {
+
+    @IBOutlet weak var coverImage: UIImageView!
+    @IBOutlet weak var showName: UILabel!
+    
+    
+}
--- Monsoon/WatchlistVC.swift
+++ Monsoon/WatchlistVC.swift
@@ -8,37 +8,98 @@
 
 import UIKit
 import Parse
+import ParseUI
 
 class WatchlistVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet weak var hasEnded: UIBarButtonItem!
-    @IBOutlet weak var allShows: UIBarButtonItem!
-    @IBOutlet weak var yetToStart: UIBarButtonItem!
+    @IBOutlet weak var watchlistBtn: UIBarButtonItem!
+    @IBOutlet weak var archivedBtn: UIBarButtonItem!
     
     @IBOutlet weak var watchlist: UITableView!
+    
+    var showList = [PFObject]()
+    //var tvShowController = TvShowController()
+    
+    let defaults = NSUserDefaults(suiteName: "group.com.bardan.monsoon")
+    var shouldFetchFromServer = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.watchlist.registerClass(UITableViewCell.self, forCellReuseIdentifier: "watchlistCell")
-        
-        hasEnded.setTitleTextAttributes([ NSFontAttributeName: UIFont(name: "Futura", size: 16)!], forState: UIControlState.Normal)
-        allShows.setTitleTextAttributes([ NSForegroundColorAttributeName: UIColor.orangeColor(), NSFontAttributeName: UIFont(name: "Futura", size: 16)!], forState: UIControlState.Normal)
-        yetToStart.setTitleTextAttributes([ NSFontAttributeName: UIFont(name: "Futura", size: 16)!], forState: UIControlState.Normal)
-        
         self.watchlist.separatorColor = UIColor.clearColor()
         self.watchlist.backgroundColor = UIColor(red: 0.93, green: 0.94, blue: 0.95, alpha: 1.0)
+        
+        // Toolbar buttons
+        
+        watchlistBtn.setTitleTextAttributes([ NSForegroundColorAttributeName: UIColor.orangeColor(), NSFontAttributeName: UIFont(name: "Futura", size: 16)!], forState: UIControlState.Normal)
+        archivedBtn.setTitleTextAttributes([ NSFontAttributeName: UIFont(name: "Futura", size: 16)!], forState: UIControlState.Normal)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        // check the last fetched date
+        let defaults = NSUserDefaults.standardUserDefaults()
+        var currentDate = NSDate()
+        
+        if let lastFetchedDate = defaults.objectForKey("lastFetchedDate") as? NSDate {
+            
+            println("Time since last fetched => \(currentDate.timeIntervalSinceDate(lastFetchedDate))")
+            
+            // only fetch from the server if it's been more than 30 minutes
+            if currentDate.timeIntervalSinceDate(lastFetchedDate) > 1800 {
+                self.shouldFetchFromServer = true
+                currentDate = NSDate()
+                defaults.setObject(currentDate, forKey: "lastFetchedDate")
+                defaults.synchronize()
+            }
+        } else {
+            println("Not set up yet...")
+            
+            // if last fetched date doesn't exist in NSUserDefaults, create one
+            defaults.setObject(currentDate, forKey: "lastFetchedDate")
+            defaults.synchronize()
+        }
+        
+        fetchShowsFromLocalStore()
+    }
+    
+    func fetchShowsFromLocalStore() {
+        
+        var query: PFQuery = PFQuery(className: "TvShow")
+        query.orderByAscending("seasonEndDate")
+        //query.fromLocalDatastore()
+        
+        query.findObjectsInBackgroundWithBlock({(NSArray objects, NSError error) in
+            if error != nil {
+                print(error)
+            } else {
+                self.showList = objects as! [PFObject]
+                //self.sortShowsByTimeLeft()
+                self.watchlist.reloadData()
+            }
+        })
+    }
+    
+    // MARK: - Table view data source
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 105
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0;
+        //return self.showList.count
+        return 1
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("watchlistCell", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("watchlistCell", forIndexPath: indexPath) as! WatchlistCell
         
-        cell.textLabel?.text = "Hello Venus!"
+        //var tvShow = self.showList[indexPath.row] as PFObject
+        //cell.textLabel?.text = tvShow["name"] as? String
         
+        cell.selectionStyle = UITableViewCellSelectionStyle.None
+        cell.backgroundColor = UIColor(red: 0.9, green: 0.93, blue: 0.95, alpha: 1.0)
+
         return cell
     }
 
